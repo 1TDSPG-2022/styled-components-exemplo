bc4f3400c7cc575e95610fe88cefd30eb6630cf4
GuilhermeRocha0 <gui.rocha009@gmail.com>
2022-09-12 12:25:04 -0300

    initial commit

---
+++ rm94771/react-aula7/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ListaTarefas from './components/listaDeTarefas/ListaTarefas'
+
+export default function App() {
+  return (
+    <div>
+      <h1>ESTILIZANDO COM STYLED-COMPONENTS</h1>
+      <ListaTarefas />
+    </div>
+  )
+}
