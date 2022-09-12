47d0089a9a58fbf653bd0e8ab32fd694b73ca079
feebueno <RM94667@fiap.com.br>
2022-09-12 11:39:24 -0300

    Ok.

--- rm94667/react-aula7/src/App.jsx
+++ rm94667/react-aula7/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Tarefa from './components/tarefa/Tarefa'
 
 export default function App(){
     return(
--- rm94667/react-aula7/src/components/tarefa/Tarefa.jsx
+++ rm94667/react-aula7/src/components/tarefa/Tarefa.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React from 'react'
+import styled from 'styled-components'
 
-export default function Tarefa(){
-    return(
+const DivTarefa = styled.div`
+    background-color : #ffb;
+    border : 2px solid #333
+    box-shadow : 5px 5px 5px #333;
+    padding: 2vh 2vw;
+    text-align:center;
+    width:50vw;
+    height:20vh;
+    margin:5vh 30vw;
+    h2,p{
+            padding-bottom:1vh
+        }
+`
+
+export default function Tarefa() {
+    return (
         <div>
-            <p>Tarefa</p>
+            <DivTarefa>
+                <p>
+                    Tarefa
+                </p>
+            </DivTarefa>
+
+
         </div>
     )
 }
