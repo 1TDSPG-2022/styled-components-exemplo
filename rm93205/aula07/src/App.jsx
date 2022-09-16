af1bb71915a4f6a9d71ee69289c02efc7131d7f8
llluferr <rm93205@fiap.com.br>
2022-09-16 08:20:02 -0300

    Criado aplicacao.

---
+++ rm93205/aula07/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ListaTarefas from './Components/ListaDeTarefas/ListaTarefas'
+
+export default function App() {
+    return (
+        <div>
+            <h1>Estilizando com styled-components</h1>
+            <ListaTarefas/>
+        </div>
+    )
+}
--- rm93205/aula07/src/index.js
+++ rm93205/aula07/src/index.js
@@ -6,6 +6,5 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,document.getElementById("root")
 );
-
