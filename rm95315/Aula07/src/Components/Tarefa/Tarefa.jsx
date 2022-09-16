24f4c7ea40142b3d65eb1e2b421e5ee29c0ac71d
brunoecp <rm95315@fiap.com.br>
2022-09-16 08:57:52 -0300

    commit

--- rm95315/Aula07/src/Components/Tarefa/Tarefa.jsx
+++ rm95315/Aula07/src/Components/Tarefa/Tarefa.jsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const DivTarefa = styled.div`       
-    background-color : #ffb;
-    border : 2px solid #333;
-    box-shadow : 5px 5px 5px #333;
-    padding: 2vh 2vw;
-    text-align:center;
-    width:50vw;
-    height:20vh;
-    margin:5vh 30vw;
-    h2,p{
-            padding-bottom:1vh
-        }
-`
+import {BtnClose, DivTarefa} from '../../Style/Styled'
 
 export default function Tarefa(props) {
     return(
         <DivTarefa>
-            <p><b>Título :</b> {props.titulo}</p>
-            <p><b>Setor :</b> {props.setor}</p>
-            <p><b>Descrição :</b> {props.descricao}</p>
+            <BtnClose>
+                <p><b>Título :</b> {props.titulo}</p>
+                <p><b>Setor :</b> {props.setor}</p>
+                <p><b>Descrição :</b> {props.descricao}</p>
+            </BtnClose>
         </DivTarefa>
     )
 }
