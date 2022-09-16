491f4708fc504d0ceb63b1d5dd8d38a9b00b5c28
tiemyz <rm95145@fiap.com.br>
2022-09-16 09:02:28 -0300

    atualizado.

--- rm95145/aula07/src/components/listaDeTarefas/ListaTarefas.jsx
+++ rm95145/aula07/src/components/listaDeTarefas/ListaTarefas.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import styled from 'styled-components'
 import Tarefa from '../tarefa/Tarefa'
 import { useState } from 'react';
+import { DivLista } from '../../style/styled'
 
-const DivLista = styled.div`
-    width: 100%;
-    min-height: 85vh;
-    background-color: #ffb;
-    padding: 20px;
-    border: 2px solid #ccc;
-    display: flex; flex-wrap: wrap;
-    justify-content: space-around;
-`
+
 export default function ListaTarefas() {
     const [tarefa, setTarefa] = useState([{
             titulo: 'Lista de pagamentos',
