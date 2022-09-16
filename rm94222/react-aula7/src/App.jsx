77be132c164c0a5256679f94150d0efcb542489e
lelemaia <rm94222@fiap.com.br>
2022-09-16 08:23:34 -0300

    ...

--- rm94222/react-aula7/src/App.jsx
+++ rm94222/react-aula7/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import ListaTarefas from './components/ListaDeTarefas/ListaTarefas'
 
 export default function App() {
     return (
-        <div>App</div>
+        <div>
+            <h1>Estilizando com styled-components</h1>
+            <ListaTarefas/>
+        </div>
     )
 }
-
