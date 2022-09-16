0dd6da0361aca790d2d0882743271dc167b59e2e
lelemaia <rm94222@fiap.com.br>
2022-09-16 10:01:49 -0300

    jk

--- rm94222/react-aula7/src/components/FormTarefas.jsx
+++ rm94222/react-aula7/src/components/FormTarefas.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 
-export default function FormTarefas() {
+export default function FormTarefas(props) {
   return (
     <div>
-        <form method='POST' onSubmit="">
+        <form method='POST' onSubmit={props.adicionarDados}>
             <div>
                 <label>Titulo</label>
-                <input type="text" name='titulo' placeholder='Titulo'/>
+                <input type="text" name='titulo' placeholder='Titulo' value = {props.novaTarefa.titulo} onChange={props.capturarDados}/>
             </div>
             <div>
                 <label>Setor</label>
-                <input type="text" name='setor' placeholder='Setor'/>
+                <input type="text" name='setor' placeholder='Setor'value = {props.novaTarefa.setor} onChange={props.capturarDados}/>
             </div>
             <div>
                 <label>Descrição</label>
-                <textarea name='descricao' cols="30" rows="10" placeholder='Descricao'></textarea>
+                <textarea name='descricao' cols="30" rows="10" placeholder='Descricao' value = {props.novaTarefa.descricao} onChange={props.capturarDados}></textarea>
             </div>
             <div>
                 <button type='submit'>Adicionar</button>
