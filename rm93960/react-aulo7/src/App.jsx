48d0dff179185690f9078762e3b2f78d503da030
lelemaia <rm94222@fiap.com.br>
2022-09-16 07:56:16 -0300

    atualização

--- rm93960/react-aulo7/src/App.jsx
+++ rm93960/react-aulo7/src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-
+import React from 'react'
+import ListaTarefas from './components/listaDeTarefas/ListaTarefas'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <h1>ESTILIZANDO COM STYLED-COMPONENTS</h1>
+      <ListaTarefas />
+    </div>
   )
 }
-
  
